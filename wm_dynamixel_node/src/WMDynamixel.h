d4b7b02ecb99a8977e3805187ef4e445b93ef035
Lucas Maurice <lucas.maurice@outlook.com>
2017-06-29 23:59:14 -0400

    Working node without security and watchdog

---
+++ wm_dynamixel_node/src/WMDynamixel.h
@@ -0,0 +1,36 @@
+//
+// Created by lucas on 29/06/17.
+//
+
+#ifndef PROJECT_WMDYNAMIXEL_H
+#define PROJECT_WMDYNAMIXEL_H
+
+#include "WMDynamixelNode.h"
+
+#define PI 3.1415926535897932
+
+class WMDynamixel {
+public:
+	WMDynamixel(int Id, double offset, int resolution);
+	void updateDynamixel(int Id, double offset, int resolution);
+	void setVelocity(double newVelocity);
+	void publishPosition(ros::Publisher pub);
+	int getID();
+	
+private:
+	//id of material dynamixel
+	int _ID;
+	
+	//the dynamixel is actif
+	bool _isEnable;
+	
+	//initial position of dynamixel
+	double _offset;
+	
+	//number of positions for 2Pi Rad in dynamixel
+	double _coeficient;
+	
+	void initDynamixel();
+};
+
+#endif //PROJECT_WMDYNAMIXEL_H
---
+++ wm_dynamixel_node/src/WMDynamixelNode.h
@@ -0,0 +1,30 @@
+//
+// Created by philippe on 03/05/17.
+//
+
+#ifndef PROJECT_WMDynamixelNode_H
+#define PROJECT_WMDynamixelNode_H
+
+#include <ros/ros.h>
+
+#include "std_msgs/MultiArrayLayout.h"
+#include "std_msgs/MultiArrayDimension.h"
+#include "std_msgs/Float64MultiArray.h"
+
+#include "dynamixel_sdk.h"
+
+#define PORTNAME "/dev/ttyUSB0"
+#define BAUDRATE 57600
+
+void nodeLoop();
+bool InitPort(const char *PortName, int BaudRate);
+void WriteAcceleration(std_msgs::Float64MultiArrayConstPtr msg);
+void addDynamixel(std_msgs::Float64MultiArrayConstPtr msg);
+void ReadFeedback();
+	
+bool write1BDynamixel(int ID, int iAddress, int iValue);
+bool write2BDynamixel(int ID, int iAddress, int iValue);
+int read1BDynamixel(int ID, int iAddress);
+int read2BDynamixel(int ID, int iAddress);
+
+#endif //PROJECT_WMDynamixelNode_H
