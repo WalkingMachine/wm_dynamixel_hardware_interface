a7a3bcc8dbbb05f2fa31fd45aae86bb937fcba86
Lucas Maurice <lucas.maurice@outlook.com>
2017-06-30 17:50:58 -0400

    Add watchdog

--- wm_dynamixel_node/src/WMDynamixel.cpp
+++ wm_dynamixel_node/src/WMDynamixel.cpp
@@ -4,14 +4,14 @@
 
 #include "WMDynamixel.h"
 WMDynamixel::WMDynamixel(int Id, double offset, int resolution) {
-	updateDynamixel(Id,offset,resolution);
+    updateDynamixel(Id, offset, resolution);
 }
 
 void WMDynamixel::initDynamixel() {
-	int pos;
-	int goal = (int)(_offset/_coeficient);
-	ROS_INFO("//set TORQUE to ON");
-	write1BDynamixel(_ID, ADDR_P1_TORQUE_ENABLE,1);
+    int pos;
+    int goal = (int) (_offset / _coefficient);
+    ROS_INFO("//set TORQUE to ON");
+    write1BDynamixel(_ID, ADDR_P1_TORQUE_ENABLE, 1);
     usleep(DELAY);
 
     /* -> FOR INITIAL POSITIONING <- */
@@ -44,38 +44,41 @@ void WMDynamixel::initDynamixel() {
     write2BDynamixel(_ID, ADDR_P1_MOVING_SPEED_2BYTES, 0);
     usleep(DELAY);
 
-	ROS_INFO("//set WHEEL mode");
-	
-	write2BDynamixel(_ID, ADDR_P1_CW_LIMIT_2BYTES,0);
-	write2BDynamixel(_ID, ADDR_P1_CCW_LIMIT_2BYTES,0);
+    ROS_INFO("//set WHEEL mode");
+
+    write2BDynamixel(_ID, ADDR_P1_CW_LIMIT_2BYTES, 0);
+    write2BDynamixel(_ID, ADDR_P1_CCW_LIMIT_2BYTES, 0);
 
     usleep(DELAY);
 }
 
 void WMDynamixel::setVelocity(double newVelocity) {
-	ROS_INFO("receive new velocity");
-	if(_isEnable){
-		ROS_INFO("read new velocity");
-		int iVelocity = (int)(newVelocity * 325.631013566);
-		
-		if(iVelocity<0){
-			iVelocity = 1023 - iVelocity;
-		}
-		
-		write2BDynamixel(_ID, ADDR_P1_MOVING_SPEED_2BYTES, iVelocity);
-	}
+    ROS_INFO("receive new velocity");
+    if (_isEnable) {
+        ROS_INFO("read new velocity");
+        int iVelocity = (int) (newVelocity * 325.631013566);
+
+        if (iVelocity < 0) {
+            iVelocity = 1023 - iVelocity;
+        }
+        write2BDynamixel(_ID, ADDR_P1_MOVING_SPEED_2BYTES, iVelocity);
+        _watchdog = time(0);
+    }
 }
 
 bool WMDynamixel::publishPosition(ros::Publisher pub) {
-	if(_isEnable){
+    if (_isEnable) {
+        //test watchdog
+        watchdogMgr();
+
         bool dxl_error = false;
-		//read datas
-		std_msgs::Float64MultiArray msg;
-		
-		//add coef
-		msg.data.push_back((double)_ID);
+        //read datas
+        std_msgs::Float64MultiArray msg;
+
+        //add coef
+        msg.data.push_back((double) _ID);
         usleep(DELAY);
-        msg.data.push_back(_coeficient * read2BDynamixel(_ID, ADDR_P1_PRESENT_POSITION_2BYTES, &dxl_error));
+        msg.data.push_back(_coefficient * read2BDynamixel(_ID, ADDR_P1_PRESENT_POSITION_2BYTES, &dxl_error));
         if (dxl_error) {
             ROS_INFO("Error reading position");
             return false;
@@ -98,23 +101,32 @@ bool WMDynamixel::publishPosition(ros::Publisher pub) {
         }
         usleep(DELAY);
 
-		//publish values
-		pub.publish(msg);
-	}
+        //publish values
+        pub.publish(msg);
+    }
     return true;
 }
 
 int WMDynamixel::getID(){
-	return _ID;
+    return _ID;
 }
 
 void WMDynamixel::updateDynamixel(int Id, double offset, int resolution) {
-	_ID = Id;
-	_isEnable = false;
-	_offset = offset;
-	_coeficient = (2*PI)/resolution;
-	ROS_INFO("Dynamixel added with ID %i, offset %f and coef %f.",_ID,_offset,_coeficient);
-	initDynamixel();
-	_isEnable = true;
-	ROS_INFO("Initialised a dynamixel with ID %i, offset %f and coef %f.",_ID,_offset,_coeficient);
+    _ID = Id;
+    _isEnable = false;
+    _offset = offset;
+    _coefficient = (2 * PI) / resolution;
+    ROS_INFO("Dynamixel added with ID %i, offset %f and coef %f.", _ID, _offset, _coefficient);
+    initDynamixel();
+    _isEnable = true;
+    ROS_INFO("Initialised a dynamixel with ID %i, offset %f and coef %f.", _ID, _offset, _coefficient);
+}
+
+bool WMDynamixel::watchdogMgr() {
+    long double sysTime = time(0);
+    if (sysTime - WATCHDOG > _watchdog) {
+        setVelocity(0);
+        return true;
+    }
+    return false;
 }
--- wm_dynamixel_node/src/WMDynamixel.h
+++ wm_dynamixel_node/src/WMDynamixel.h
@@ -6,9 +6,11 @@
 #define PROJECT_WMDYNAMIXEL_H
 
 #include "WMDynamixelNode.h"
+#include <ctime>
 
 #define PI 3.1415926535897932
 #define DELAY 10000
+#define WATCHDOG 500000
 
 class WMDynamixel {
 public:
@@ -16,6 +18,7 @@ public:
 	void updateDynamixel(int Id, double offset, int resolution);
 	void setVelocity(double newVelocity);
 
+    bool watchdogMgr();
     bool publishPosition(ros::Publisher pub);
 	int getID();
 	
@@ -30,8 +33,11 @@ private:
 	double _offset;
 	
 	//number of positions for 2Pi Rad in dynamixel
-	double _coeficient;
-	
+    double _coefficient;
+
+    //last write time
+    long double _watchdog;
+
 	void initDynamixel();
 };
 
